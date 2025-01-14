d8989a93de4d88336f67119b53285924d8e84bd4
Joanna May <joannamadisonmay@gmail.com>
2025-01-14 01:31:06 -0600

    wip: codesign dylibs

--- build.sh
+++ build.sh
@@ -12,23 +12,18 @@ fi
 case "$1" in
   "macos")
     echo "Publishing for macOS..."
-    dotnet publish Chickensoft.Platform -v:detailed -o ./addons/platform/builds/macos --self-contained -c Debug -r osx-arm64
     dotnet publish Chickensoft.Platform -v:detailed -o ./addons/platform/builds/macos --self-contained -c Release -r osx-arm64
-    dotnet publish Chickensoft.Platform -v:detailed -o ./addons/platform/builds/macos --self-contained -c Debug -r osx-x64
     dotnet publish Chickensoft.Platform -v:detailed -o ./addons/platform/builds/macos --self-contained -c Release -r osx-x64
     ;;
 
   "windows")
     echo "Publishing for Windows..."
-    dotnet publish Chickensoft.Platform -v:detailed -o ./addons/platform/builds/windows --self-contained -c Debug -r win-x64
     dotnet publish Chickensoft.Platform -v:detailed -o ./addons/platform/builds/windows --self-contained -c Release -r win-x64
     ;;
 
   "linux")
     echo "Publishing for Linux..."
-    dotnet publish Chickensoft.Platform -v:detailed -o ./addons/platform/builds/linux --self-contained -c Debug -r linux-x64
     dotnet publish Chickensoft.Platform -v:detailed -o ./addons/platform/builds/linux --self-contained -c Release -r linux-x64
-    dotnet publish Chickensoft.Platform -v:detailed -o ./addons/platform/builds/linux --self-contained -c Debug -r linux-arm64
     dotnet publish Chickensoft.Platform -v:detailed -o ./addons/platform/builds/linux --self-contained -c Release -r linux-arm64
     ;;
 
