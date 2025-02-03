9cef9018d54237d40696128ba440b27282f5910d
Joanna May <joannamadisonmay@gmail.com>
2025-02-03 17:21:58 -0600

    wip: calculate correction factor from monitor scale to system scale

--- sandbox/Chickensoft.Platform.SandboxRef/src/Main.cs
+++ sandbox/Chickensoft.Platform.SandboxRef/src/Main.cs
@@ -3,6 +3,8 @@ namespace Chickensoft.Platform.SandboxRef;
 using Godot;
 
 public partial class Main : Node2D {
+  public const float THEME_SCALE = 4f;
+
   public override void _Ready() {
     var displays = new Displays();
 
@@ -17,24 +19,27 @@ public partial class Main : Node2D {
       ProjectSettings.GetSetting("display/window/size/viewport_height").AsInt32()
     );
     var resolution = displays.GetNativeResolution(window);
-    var scaleFactor = displays.GetDisplayScaleFactor(window);
-    var godotDpi = DisplayServer.ScreenGetDpi(window.CurrentScreen);
-    var godotScaleFactor = godotDpi / 96.0f;
+    var monitorScale = displays.GetDisplayScaleFactor(window);
+    var systemDpi = DisplayServer.ScreenGetDpi(window.CurrentScreen);
+    var systemScale = systemDpi / 96.0f;
     var godotRes = DisplayServer.ScreenGetSize(window.CurrentScreen);
     var windowSize = window.Size;
+    var contentScaleFactor = monitorScale / THEME_SCALE;
+    var correctionFactor = monitorScale / systemScale;
 
     // The native resolution (true resolution of the monitor) and Godot's
     // understanding of the monitor resolution on Windows can be different,
     // since Godot does not have per-monitor DPI awareness on Windows (yet).
     // https://github.com/godotengine/godot/issues/56341
-    GD.Print($"Native Resolution: {resolution.X}, {resolution.Y}");
-    GD.Print($"Godot  Resolution: {godotRes.X}, {godotRes.Y}");
-    GD.Print($"            Scale: {scaleFactor}");
-    GD.Print($"        Godot DPI: {godotDpi}");
-    GD.Print($"      Godot Scale: {godotScaleFactor}");
-    GD.Print($"           Window: {windowSize.X}, {windowSize.Y}");
+    GD.Print($"   Native Resolution: {resolution.X}, {resolution.Y}");
+    GD.Print($"   Godot  Resolution: {godotRes.X}, {godotRes.Y}");
+    GD.Print($"  True Monitor Scale: {monitorScale}");
+    GD.Print($"        System Scale: {systemScale}");
+    GD.Print($"              Window: {windowSize.X}, {windowSize.Y}");
+    GD.Print($"Content Scale Factor: {contentScaleFactor}");
+    GD.Print($"   Correction Factor: {correctionFactor}");
     GD.Print(
-      $"   Project Window: {projectWindowSize.X}, {projectWindowSize.Y}"
+      $"      Project Window: {projectWindowSize.X}, {projectWindowSize.Y}"
     );
   }
 }
